463973bfad03ee7d67f8ff613820a378e9118aea
0x1 <kevin.francart3@gmail.com>
2026-08-15 18:35:52 +0200

    misc

--- test_dump_classdecls.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('output/Data.Ord/corefn.json', 'utf8'));
-const ord = data.classDecls.find(c => c.name === 'Ord');
-console.log(JSON.stringify(ord, null, 2));
